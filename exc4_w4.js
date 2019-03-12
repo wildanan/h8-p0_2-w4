1cf5c4a4380be7ae3e87dd584ad137ce9f66f7ef
wildan <wildanagung34@gmail.com>
2019-03-12 22:50:44 +0700

    coba

---
+++ exc4_w4.js
@@ -0,0 +1,22 @@
+function cariModus(arr) {
+    var modus = []
+    for (var i = 0; i < arr.length; i++) {
+      for (var j = i + 1; j < arr.length; j++) {
+        if (arr[i] === arr[j]) {
+          modus.push(arr[i])
+        }
+      }
+    }
+    if (modus[0] === undefined || modus.length === arr.length * 2 ) {
+      return -1
+    } 
+    return modus[0]
+  }
+
+  
+  // TEST CASES
+  console.log(cariModus([10, 4, 5, 2, 4])); // 4
+  console.log(cariModus([5, 10, 10, 6, 5])); // 5
+  console.log(cariModus([10, 3, 1, 2, 5])); // -1
+  console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+  console.log(cariModus([7, 7, 7, 7, 7])); // -1
